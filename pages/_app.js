be41393410fe13b466dca1e557a0b9a4a92fba43
Siddhesh Kankekar <kankekas@tcd.ie>
2021-06-08 13:56:49 +0100

    initialize nextauth  app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading] = useAuthState(auth);
+
+  // if (loading) return <h1>Loading...</h1>;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
